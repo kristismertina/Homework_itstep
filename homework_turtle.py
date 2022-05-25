bb0161b8fb4213666be28d3a90661934cf20b96a
kristismertina <84023743+kristismertina@users.noreply.github.com>
2022-05-25 14:39:08 +0300

    Add files via upload

---
+++ homework_turtle.py
@@ -0,0 +1,58 @@
+password = '11'
+a = 0
+while a < 5:
+    password_inter = input()
+    if password == password_inter:
+        print('Верно. Добро пожаловать!')
+        break
+    else:
+        print('Неверный пароль')
+        a += 1
+
+
+import random
+
+num = random.randint(1,100)
+count = 0
+
+while count <= 10:
+    num_inter = int(input())
+    if num == num_inter:
+        print('You win')
+        break
+    else:
+        print('Try again')
+        count += 1
+
+
+import turtle
+
+turtle.shape("turtle")
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+
+import turtle
+
+turtle.shape("turtle")
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+
+
+
+
